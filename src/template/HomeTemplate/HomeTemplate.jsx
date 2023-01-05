e66e5af2eee86b2101088dc81785f597470f1488
Nam Duong <duongquocnam224400@gmail.com>
2023-01-05 16:22:05 +0700

    using scrool-to-top

--- src/template/HomeTemplate/HomeTemplate.jsx
+++ src/template/HomeTemplate/HomeTemplate.jsx
@@ -2,10 +2,11 @@ import React, { Fragment } from 'react';
 import Footer from '~/layouts/Footer';
 import Header from '~/layouts/Header';
 import Navbar from '~/layouts/Navbar';
-
+import ScrollToTop from 'react-scroll-to-top';
 const HomeTemplate = ({ children }) => {
   return (
     <div className="">
+      <ScrollToTop smooth color="#fff" />
       <Header />
       <Navbar />
       <Fragment>{children}</Fragment>
